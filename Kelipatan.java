edca8f3081712dee76fd3b57d607cf1b1013c26a
AstiNabila <nabilaasti78@gmail.com>
2024-11-04 20:25:58 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan.pkg5;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kelipatan {
+    public static void main(String[] args) {
+        double awal = 3.5;
+        double akhir = 35.0;
+
+        for (double i = awal; i <= akhir; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
+
+
